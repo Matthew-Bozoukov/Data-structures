e46bf0fe590f6e8b055288d0b6c179c27c82de5a
1861685 <1861685@01-19813.pusd.dom>
2022-01-19 10:11:10 -0800

    data structures stack and queue

---
+++ Data_structures/src/queue.java
@@ -0,0 +1,52 @@
+
+
+
+	public class queue<T> extends stack<T>{
+		private stack<T> st1;
+		private stack<T> st2;
+		private int size;
+
+	public queue(stack<T> st1, stack<T> st2) {
+		this.st1=st1;
+		this.st2=st2;
+		size=0;
+		
+		
+	}
+	public void add(T el) {
+	st1.push(el);
+	size+=1;
+	
+	
+		}
+	
+	public T remove() {
+		if(st2.isEmpty()) {
+			T temp=st1.pop();
+			size-=1;
+			st2.push(temp);
+			return st2.pop();
+		}else {
+			return st2.pop();
+		}
+		
+	}
+	public String toString() {
+		return null;
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		stack<Integer> st1=new stack<Integer>();
+		stack<Integer> st2= new stack<Integer>();
+		
+		queue<Integer> qu = new queue<Integer>(st1, st2);
+		qu.add(1);
+		qu.add(2);
+		
+		
+	}
+
+}
+
---
+++ Data_structures/src/stack.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class stack<T> {
+private ArrayList<T> data; //int[] data, String[] mystr
+private int size; // # of elements
+	public stack(){
+		data=new ArrayList<T>();
+		size=0;
+		
+		
+		
+	}
+	
+	public int size() {
+		return size;
+	}
+	public void push(T el) {
+		data.add(el);
+		size+=1;//update size
+	}
+	public T pop() {
+		 //remove and return the element
+		data.remove(size-1);
+		return data.get(size-1);
+
+		//update other relevant info
+		
+	
+	}
+	public boolean isEmpty() {
+		if(size==0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public String toString() {
+		
+		return data.get(data.size())+ "";
+	}
+}
+
+
+
+
+	//push - add element
+	
+	//pop remove and return element from the list
+	//should be from he "top" of the list
+	//public void main(String[] args) {
+		// TODO Auto-generated method stub
+		//add lines of code to test implementation of stack works
+		
+		//stack st = new stack();
+		//st.toString();
+	
+		
+
