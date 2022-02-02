4c61c10908057b6505797975e08ecb8068a76a14
1861685 <1861685@01-19813.pusd.dom>
2022-02-02 09:14:47 -0800

    commit and push

---
+++ Data_structures/src/linkedList.java
@@ -0,0 +1,9 @@
+
+public class linkedList {
+//kind of like a chain every link is a node
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}//ss
--- Data_structures/src/queue.java
+++ Data_structures/src/queue.java
@@ -2,7 +2,7 @@ public class queue<T> extends stack<T>{
 		private stack<T> st1;
 		private stack<T> st2;
 		private int size1;
-		private int size2;
+		
 
 	public queue() {
 		st1= new stack<T>();
@@ -15,18 +15,19 @@ public class queue<T> extends stack<T>{
 			size1+=1;
 			
 			
+			
 		}
 	public T remove() {
 		if(st2.isEmpty()) {
-		for(int i=0; i<size1; i++) {
+		while(!st1.isEmpty()) {
 			st2.push(st1.pop());
 			size1-=1;
 		}
-		return st2.pop();
-		}else {
-			return st2.pop();
+		
+		
 	
 		}
+		return st2.pop();
 		
 	}
 	
@@ -35,34 +36,22 @@ public class queue<T> extends stack<T>{
 	
 	
 	public String toString() {
-	return st2.toString( )+ st1.toString();
-	
+	return st2.toString();
 	}
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//stack<Integer> st1=new stack<Integer>();
-		//stack<Integer> st2= new stack<Integer>();
+		
 		
 	queue<Integer> qu = new queue<Integer>();
+		qu.add(3);
+		qu.add(2);
 		qu.add(1);
-	qu.add(2);
-	qu.add(3);
-	qu.add(4);
-	qu.remove();
-	
-	
-	
-	
-		
-	
-		
-		
-	System.out.println(qu);
+		System.out.println(qu.remove());
+		System.out.println(qu.remove());
+		System.out.println(qu.remove());
 		
 		
 	}
 	}
-
-
